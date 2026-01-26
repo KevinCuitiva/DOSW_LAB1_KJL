d0816f24567e1b3f8addde8c91acd33324bc7aaf
JuanSilvaaa <juan.silva-c@mail.escuelaing.edu.co>
2026-01-26 17:06:47 -0500

    Carril 1: Función lambda para encontrar número mayor

---
+++ laboratorio1/reto2/CarreraParalelo.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class CarreraParalelo {
+    
+    static class Resultados {
+        int numeroMayor;
+        int numeroMenor;
+        int cantidadDatos;
+        boolean esParODivisible;
+        
+        public Resultados(int numeroMayor, int numeroMenor, int cantidadDatos, boolean esParODivisible) {
+            this.numeroMayor = numeroMayor;
+            this.numeroMenor = numeroMenor;
+            this.cantidadDatos = cantidadDatos;
+            this.esParODivisible = esParODivisible;
+        }
+        
+        @Override
+        public String toString() {
+            return "Resultados{" +
+                    "numeroMayor=" + numeroMayor +
+                    ", numeroMenor=" + numeroMenor +
+                    ", cantidadDatos=" + cantidadDatos +
+                    ", esParODivisible=" + esParODivisible +
+                    '}';
+        }
+    }
+    
+    // Interfaz funcional para operación con lista de números
+    @FunctionalInterface
+    interface OperacionNumeros {
+        int operar(List<Integer> numeros);
+    }
+    
+    // Carril 1 - Encuentra el número más grande
+    public static OperacionNumeros encontrarMayor = lista -> {
+        if (lista == null || lista.isEmpty()) return Integer.MIN_VALUE;
+        return lista.stream().max(Integer::compare).orElse(Integer.MIN_VALUE);
+    };
+    
+    public static void main(String[] args) {
+        // Aquí irán las pruebas de los dos carriles
+    }
+}
