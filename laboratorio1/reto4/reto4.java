48933b28892a8278c9df35b8c67ec3f12e5b5cde
Laura Castillo <laura.castillo-f@mail.escuelaing.edu.co>
2026-01-26 19:29:50 -0500

    Hashtable synchronizzed and ascending order

--- laboratorio1/reto4/reto4.java
+++ laboratorio1/reto4/reto4.java
@@ -1,3 +1,24 @@
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+
 public class reto4 {
 
+    public static Map<String, Integer> crearHashtable(List<Map.Entry<String, Integer>> lista) {
+        Map<String, Integer> hashtable = new Hashtable<>();
+
+        for (Map.Entry<String, Integer> entrada : lista) {
+            hashtable.put(entrada.getKey(), entrada.getValue());
+        }
+
+        return hashtable;
+    }
+
+    public static void imprimirOrdenado(Map<String, Integer> mapa) {
+        mapa.entrySet().stream()
+            .sorted(Map.Entry.comparingByKey())
+            .forEach(e ->
+                System.out.println("Clave: " + e.getKey() + " | Valor: " + e.getValue())
+            );
+    }
 }
