d4dcc7fb0dbf8bb30e586a4b3a73dd0ecb4824c5
Laura Castillo <laura.castillo-f@mail.escuelaing.edu.co>
2026-01-26 19:08:47 -0500

    a list is received and stored in a hashmap ignoring duplicates

--- laboratorio1/reto4/reto4.java
+++ laboratorio1/reto4/reto4.java
@@ -1,3 +1,23 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class reto4 {
 
+    public static Map<String, Integer> crearHashMap(List<Map.Entry<String, Integer>> lista) {
+        Map<String, Integer> hashMap = new HashMap<>();
+
+        for (Map.Entry<String, Integer> entrada : lista) {
+            hashMap.putIfAbsent(entrada.getKey(), entrada.getValue());
+        }
+
+        return hashMap;
+    }
+
+    // Función extra del estudiante A
+    public static void imprimirEnMayusculas(Map<String, Integer> mapa) {
+        mapa.forEach((k, v) ->
+            System.out.println("Clave: " + k.toUpperCase() + " | Valor: " + v)
+        );
+    }
 }
