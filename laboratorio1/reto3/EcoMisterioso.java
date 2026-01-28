2c070a699ba8cb4426867bd04639f1fcbf39767f
JuanSilvaaa <juan.silva-c@mail.escuelaing.edu.co>
2026-01-28 09:02:05 -0500

    Add StringBuilder repeat method

---
+++ laboratorio1/reto3/EcoMisterioso.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.util.stream.*;
+
+public class EcoMisterioso {
+
+    @FunctionalInterface
+    interface Transformacion {
+        String procesar(String mensaje);
+    }
+
+    public static Transformacion repetirConBuilder = mensaje -> {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 3; i++) {
+            sb.append(mensaje);
+            if (i < 2) sb.append(" ");
+        }
+        return sb.toString();
+    };
+
+    public static void main(String[] args) {
+        
+    }
+}
