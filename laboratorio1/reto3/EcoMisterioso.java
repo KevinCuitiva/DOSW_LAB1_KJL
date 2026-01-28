b4b11934e7697a3a0060f6b8763f88fed16c4e4a
JuanSilvaaa <juan.silva-c@mail.escuelaing.edu.co>
2026-01-28 08:59:04 -0500

    Estudiante A: Estructura base del reto 3 - El eco misterioso

---
+++ laboratorio1/reto3/EcoMisterioso.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.stream.*;
+
+public class EcoMisterioso {
+
+    @FunctionalInterface
+    interface Transformacion {
+        String procesar(String mensaje);
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
